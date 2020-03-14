a4efb2d760cd5fef3eb8c38126ff93b6cde7968d
Kyle Finley <kyle@finley.sh>
2020-03-14 11:08:26 -0700

    move resource definition to a dedicated object

--- cfngin_bucket/blueprints/cfngin_bucket.py
+++ cfngin_bucket/blueprints/cfngin_bucket.py
@@ -1,36 +1,10 @@
-"""Creates an S3 Bucket for CFNgin.
-
-Variables
----------
-
-**BucketName (Optional[str])**
-    Name to use for the S3 Bucket. If not provided, the name will be
-    ``<stack-name>-<region>``. (`default:` ``None``)
-
-**KMSMasterKeyID (Optional[str])**
-    Key ID or ARN for a KMS key to use for ServerSideEncryptionByDefault_.
-    If not provided, the default S3 service key will be used.
-    (`default:` ``None``)
-
-**Tags (Dict[str, str])**
-    Mapping for {tag-key: tag-value} for the Bucket. (`default:` ``{}``)
-
-.. _ServerSideEncryptionByDefault: https://tinyurl.com/vub9cej
-
-Outputs
--------
-
-**CFNginBucket (str)**
-    Name of the S3 Bucket that was created.
-
-**CFNginBucketArn (str)**
-    ARN of the S3 Bucket that was created.
-
-"""
+"""Creates an S3 Bucket for CFNgin."""
 from typing import Any, Dict
 
 from runway.cfngin.blueprints.base import Blueprint
-from troposphere import GetAtt, Tags, Template, s3
+from troposphere import Template, AWSObject
+
+from .resources.s3 import CFNginBucket
 
 TEMPLATE_DESCRIPTION = 'Runway CFNgin Bucket'
 
@@ -38,65 +12,24 @@ TEMPLATE_DESCRIPTION = 'Runway CFNgin Bucket'
 class BlueprintClass(Blueprint):
     """Extends CFNgin's Blueprint class."""
 
-    VARIABLES: Dict[str, Any] = {
-        'BucketName': {'type': (str, type(None)), 'default': None},
-        'KMSMasterKeyID': {'type': (str, type(None)), 'default': None},
-        'Tags': {'type': dict, 'default': {}}
-    }
+    VARIABLES: Dict[str, Any] = CFNginBucket.VARIABLES
 
     @property
-    def bucket_name(self) -> str:
-        """CFNgin Bucket name."""
-        if self.vars['BucketName']:
-            return self.vars['BucketName']
-        # using 'bucket_region' for the time being until there is something more reliable
-        return f"{self.name.lower()}-{self.context.bucket_region}"
+    def fqn(self) -> str:
+        """Fully qualified name of the stack that will be produced."""
+        return self.context.get_fqn(self.name)
 
-    @property
-    def vars(self) -> Dict[str, Any]:
-        """Blueprint variables.
-
-        Returns:
-            Resolved variables.
-
-        """
-        return self.get_variables()
-
-    def bucket(self) -> s3.Bucket:
-        """CFNgin Bucket resource."""
-        return s3.Bucket(
-            'CFNginBucket',
-            BucketEncryption=self.encryption_settings(),
-            BucketName=self.bucket_name,
-            Tags=Tags(**self.vars['Tags']),
-            # not parameterized b/c this shouldn't need to be paused
-            VersioningConfiguration=s3.VersioningConfiguration(Status='Enabled')
-        )
-
-    def encryption_settings(self,
-                            kms_key_id: str = None
-                            ) -> s3.BucketEncryption:
-        """CFNgin Bucket encryption settings.
+    def add_resource(self, obj: AWSObject) -> None:
+        """Add a resource object to the Blueprint.
 
         Args:
-            kms_key_id: Key ID or ARN for a KMS key to use for default Bucket
-                encryption. The default S3 service key will be used if not
-                provided.
-
-        Returns:
-            S3 Bucket encryption settings.
-
+            obj: Subclass of a Troposphere resource that provisions
+                itself during initialization.
         """
-        kwargs = {'SSEAlgorithm': 'AES256'}
-        if not kms_key_id:
-            kms_key_id = self.vars['KMSMasterKeyID']
-        if kms_key_id:
-            kwargs = {'KMSMasterKeyID': kms_key_id,
-                      'SSEAlgorithm': 'aws:kms'}
-        return s3.BucketEncryption(
-            ServerSideEncryptionConfiguration=[s3.ServerSideEncryptionRule(
-                ServerSideEncryptionByDefault=s3.ServerSideEncryptionByDefault(
-                    **kwargs))])
+        obj(context=self.context,
+            stack_fqn=self.fqn,
+            template=self.template,
+            variables=self.get_variables())
 
     def create_template(self) -> None:
         """Create template (method called by CFNgin)."""
@@ -104,7 +37,4 @@ class BlueprintClass(Blueprint):
         template.set_description(TEMPLATE_DESCRIPTION)
         template.set_version('2010-09-09')
 
-        bucket = template.add_resource(self.bucket())
-
-        self.add_output(bucket.title, bucket.ref())
-        self.add_output(f'{bucket.title}Arn', GetAtt(bucket, 'Arn'))
+        self.add_resource(CFNginBucket)
---
+++ cfngin_bucket/blueprints/resources/s3.py
@@ -0,0 +1,103 @@
+"""S3 resources for this CFNgin module."""
+from typing import Any, Dict
+
+from runway.cfngin.context import Context
+from troposphere import Output, Tags, Template, s3
+
+
+class CFNginBucket(s3.Bucket):
+    """S3 Bucket used for CFNgin cacheing.
+
+    VARIABLES
+    ---------
+
+    **BucketName (Optional[str])**
+        Name to use for the S3 Bucket. If not provided, the name will be
+        ``<stack-name>-<region>``. (`default:` ``None``)
+
+    **KMSMasterKeyID (Optional[str])**
+        Key ID or ARN for a KMS key to use for ServerSideEncryptionByDefault_.
+        If not provided, the default S3 service key will be used.
+        (`default:` ``None``)
+
+    **Tags (Dict[str, str])**
+        Mapping for {tag-key: tag-value} for the Bucket. (`default:` ``{}``)
+
+    .. _ServerSideEncryptionByDefault: https://tinyurl.com/vub9cej
+
+    Outputs
+    -------
+
+    **CFNginBucket (str)**
+        Name of the S3 Bucket that was created.
+
+    **CFNginBucketArn (str)**
+        ARN of the S3 Bucket that was created.
+
+    """
+
+    LOGICAL_NAME = 'CFNginBucket'
+    VARIABLES: Dict[str, Any] = {
+        'BucketName': {'type': (str, type(None)), 'default': None},
+        'KMSMasterKeyID': {'type': (str, type(None)), 'default': None},
+        'Tags': {'type': dict, 'default': {}}
+    }
+
+    def __init__(self, *,
+                 context: Context,
+                 stack_fqn: str,
+                 template: Template,
+                 variables: Dict[str, Any],
+                 **kwargs: Any) -> None:
+        """Instantiate class.
+
+        Keyword Args:
+            context: CFNgin context object.
+            stack_fqn: Fully qualified name of the Stack this resource is
+                being created in.
+            template: Troposphere template object.
+            variables: Resolved CFNgin variables from a Blueprint.
+
+        """
+        self._context = context
+        self._vars = variables
+        self._stack_fqn = stack_fqn
+
+        super().__init__(title=self.LOGICAL_NAME,
+                         template=template,
+                         BucketEncryption=self._get_encryption_settings(),
+                         BucketName=self._get_bucket_name(),
+                         Tags=Tags(**self._vars.get('Tags', {})),
+                         VersioningConfiguration=s3.VersioningConfiguration(
+                             Status='Enabled'))
+        self._add_outputs()
+
+    def _add_outputs(self) -> None:
+        """Add outputs to the template for this resource."""
+        self.template.add_output(Output(self.title,
+                                        Value=self.ref()))
+        self.template.add_output(Output(f'{self.title}Arn',
+                                        Value=self.get_att('Arn')))
+
+    def _get_bucket_name(self) -> str:
+        """Return the name to use for the bucket."""
+        provided = self._vars.get('BucketName')
+        return provided if provided else \
+            f'{self._stack_fqn.lower()}-{self._context.bucket_region}'
+
+    def _get_encryption_settings(self) -> s3.BucketEncryption:
+        """Get CFNgin Bucket encryption settings.
+
+        Returns:
+            S3 Bucket encryption settings.
+
+        """
+        kwargs = {'SSEAlgorithm': 'AES256'}
+        kms_key_id = self._vars.get('KMSMasterKeyID')
+        if kms_key_id:
+            kwargs = {'KMSMasterKeyID': kms_key_id,
+                      'SSEAlgorithm': 'aws:kms'}
+        return s3.BucketEncryption(
+            ServerSideEncryptionConfiguration=[s3.ServerSideEncryptionRule(
+                ServerSideEncryptionByDefault=s3.ServerSideEncryptionByDefault(
+                    **kwargs))])
